d269a64cdd2e8175649370266625b83c96d0a07c
Anton <Anton@DESKTOP-K50ENP4>
2019-12-05 00:21:58 +0100

    test

--- Connect 4/src/connect4/Connect4.java
+++ Connect 4/src/connect4/Connect4.java
@@ -7,6 +7,7 @@ public class Connect4 {
 	 public static void main(String[] args) {
 	      Display test = new Display();
 	      Player player1 = new Player();
+	      Player player2 = new Player();
 	      
 	   }
 	
