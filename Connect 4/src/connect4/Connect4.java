d4f76794fe8a950d52cb7f41a9d448fec4b56769
Anton <Anton@DESKTOP-K50ENP4>
2019-12-04 22:52:19 +0100

    habe alle klassen reingetan was wir brauche, schaut euch Connect4 klasse wie das

---
+++ Connect 4/src/connect4/Connect4.java
@@ -0,0 +1,9 @@
+package connect4;
+
+public class Connect4 {
+	
+	
+	
+	Display test = new Display();
+
+}
---
+++ Connect 4/src/connect4/Player.java
@@ -0,0 +1,26 @@
+package connect4;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class Player {
+	
+	
+	
+	private String playername;
+	
+	
+	
+	
+	public void printplayername() {
+		
+		
+		System.out.println(playername);
+		
+		
+	}
+	
+}
