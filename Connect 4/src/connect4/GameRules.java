2c2436af8c345bf0db4578875b75a6c640c9a294
burhanozel <burhanozel@bozel.fritz.box>
2019-12-12 01:25:15 +0100

    game rule update! by burhan

--- Connect 4/src/connect4/GameRules.java
+++ Connect 4/src/connect4/GameRules.java
@@ -1,5 +1,34 @@
 package connect4;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 public class GameRules {
+	
+	public static boolean isFour([][] values) {
+		
+		int numberOfRows = values.length;
+		int numberOfColumns = values.length;
+		
+		//Check row
+		for (int i= 0; i < numberOfRows; i++) {
+			if (isConsecutiveFour(value[i]))
+				return true;
+		}
+		
+		//Check Colums
+		for (int j= 0; j < numberOfColumns; j++) {
+			char[] column = new char[numberOfRows];
+			
+			
+		}
+		
+		//Check lowerPart
+		for (int i=0; i< numberOfRows -3; i++) { 
+			int diagonalElements = Math.min(numberOfRows -i, numberOfColumns);
+		}
+		
+	}
 
 }
