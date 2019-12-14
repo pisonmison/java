a652fe97ddae51fc7cdb16f9dd68e519e942b9a9
anton437 <Anton@antons-air.fritz.box>
2019-12-14 15:33:31 +0100

    gridArray hat jetzt ein 6x7 spielfeld mit "-" zeichen gefüllt !

--- Connect 4/src/connect4/GridArray.java
+++ Connect 4/src/connect4/GridArray.java
@@ -2,9 +2,38 @@ package connect4;
 
 public class GridArray {
 	
-	int[] gameGridArray = new int[6]; 
+	
+	public GridArray() {
+		createArray();
+	}
+	
+	
+	
+	public void createArray() {
+	char[][] gameGridArray =  {
+	{'-','-', '-', '-', '-', '-', '-'},
+	{'-','-', '-', '-', '-', '-', '-'},
+	{'-','-', '-', '-', '-', '-', '-'},  //spielfeld
+	{'-','-', '-', '-', '-', '-', '-'},
+	{'-','-', '-', '-', '-', '-', '-'},
+	{'-','-', '-', '-', '-', '-', '-'}};
+	
 	
 	//array mit - füllen und jweils "0" oder "X" für farbe einsetzen und dann kann man rechnen
-	gameGridArray[1] = new int[3];
+	for(int i = 0; i < gameGridArray.length; i++) {
+		for (int j = 0; j < gameGridArray[i].length; j++) {
+			System.out.print(gameGridArray[i][j] + " ");
+		}
+		System.out.println("");
+		
+		
+	}
 	
+}
+	
+	
+	public static void main(String[] args) {
+		
+		new GridArray();
+	}
 }
